684a34a3c46ce09f44a65d6a813375df70d656a3
ŁukaszBorucki <lukasz.borucki@sienn.pl>
2018-07-24 09:31:20 +0200

    Initial commit

---
+++ src/main/java/co/borucki/EasyKanbanPortalBackend/EasyKanbanPortalBackendApplication.java
@@ -0,0 +1,12 @@
+package co.borucki.EasyKanbanPortalBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyKanbanPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyKanbanPortalBackendApplication.class, args);
+	}
+}
